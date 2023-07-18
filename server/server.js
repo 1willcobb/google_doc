73ea58728521bd1cf1c43fd366e8033f1cc5cf35
1willcobb <cobb.will@gmail.com>
2023-07-18 15:12:17 -0700

    attempting to hookup heroku and added express to the server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
-const mongoose = require("mongoose")
-const Document = require("./Document")
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+const mongoose = require('mongoose');
+const Document = require('./Document');
 
-mongoose.connect('mongodb://localhost/docdb',
-  {
-    useNewUrlParser: true,
-  }
-);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
 
-const io = require('socket.io')(3001, {
-    cors: {
-        origin: 'http://localhost:3000',
-        method: ['GET', 'POST']
-    }
-})
+const PORT = process.env.PORT || 3001;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/docdb', {
+  useNewUrlParser: true,
+});
 
 const defaultValue = ""
 
@@ -41,3 +48,15 @@ async function findOrCreateDocument(id) {
     if (document) return document
     return await Document.create({_id: id, data: defaultValue})
 }
+
+// Serve the static React build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
